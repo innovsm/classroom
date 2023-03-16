dd2d46667d8f4236a74c1c77cccd076a7c861cdf
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-03-16 18:46:36 +0530

    deep face

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import regestration, attendence, details
-#  ----------------  main.py ---------------
+from deepface import DeepFace
 
-st.set_page_config(page_title="attendace system", layout="wide")
-
-# -- creating the main page
-
-dict_1 = {"Student Regestration": regestration,"Attendance": attendence,"details": details}
-
-# making a sidebar and displaying the apps
-
-with st.sidebar:
-    radio = st.radio(label="change the app here", options= list(dict_1.keys()))
-
-dict_1[radio].app()  # calling differnt pages
+st.write(DeepFace.verify("test.jpg", "gama.jpg"))
