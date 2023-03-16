e61c7d46c6d312ca480b338672dbf1030908bb97
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-03-16 19:12:47 +0530

    .

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import streamlit as st
 from deepface import DeepFace
 
-st.write(DeepFace.verify("test.jpg", "gama.jpg"))
+
+from deepface import DeepFace
+
+# Load the VGG-Face model weights
+model = DeepFace.build_model('VGG-Face')
+
+# Verify two faces using the VGG-Face model
+result = DeepFace.verify("test.jpg", "gama.jpg", model=model)
+
+# Print the verification result
+st.write(result)
